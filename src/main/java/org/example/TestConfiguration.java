1d6ff255d6b80b32451b597747208b75b99ac7eb
Martin Lynch <martin.lynch1@nhs.net>
2024-07-19 15:09:43 +0100

    Initial commit

---
+++ src/main/java/org/example/TestConfiguration.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+public class TestConfiguration extends Configuration {
+    @Valid
+    @NotNull
+    private final SwaggerBundleConfiguration swagger =
+            new SwaggerBundleConfiguration();
+    @JsonProperty("swagger")
+    public SwaggerBundleConfiguration getSwagger() {
+        swagger.setResourcePackage("org.example.controllers");
+        String[] schemes = {"http", "https"};
+        swagger.setSchemes(schemes);
+        return swagger;
+    }
+}
